1aa85d8f4c0fe29ea860467a591fa8ff8b92fd95
edggard <edggard@gmail.com>
2021-08-04 19:37:14 -0300

    Primeiro Commit do Projeto Cadastro Funcionario Console

---
+++ Projeto02/Controllers/FuncionarioController.cs
@@ -0,0 +1,79 @@
+﻿using Projeto02.Entities;
+using Projeto02.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02.Controllers
+{
+    public class FuncionarioController
+    {
+        //Método que faz usuário entrar com os dados do funcionario para cadastro
+        public void CadastrarFuncionario()
+        {
+            Console.WriteLine("\n --- CADASTRO DE FUNCIONÁRIO --- \n");
+
+            //Criando obj da classe funcionário
+            var funcionario = new Funcionario();
+
+            //Instanciando relacionamentos contidos
+            funcionario.Setor = new Setor();
+            funcionario.Dependentes = new List<Dependente>();
+
+            //Inicia cadastro...
+            funcionario.IdFuncionario = Guid.NewGuid();
+
+            Console.Write("Nome do Funcionário......: ");
+            funcionario.Nome = Console.ReadLine();
+
+            Console.Write("Informe o CPF............: ");
+            funcionario.Cpf = Console.ReadLine();
+
+            Console.Write("Informe a Matrícula......: ");
+            funcionario.Matricula = Console.ReadLine();
+
+            Console.Write("Data de Admissão.........: ");
+            funcionario.DataAdmissao = DateTime.Parse(Console.ReadLine());
+
+            //Dados do SETOR (1 para 1)
+            funcionario.Setor.IdSetor = Guid.NewGuid();
+
+            Console.Write("Nome do Setor............: ");
+            funcionario.Setor.Nome = Console.ReadLine();
+
+            Console.Write("Descrição do Setor.......: ");
+            funcionario.Setor.Descricao = Console.ReadLine();
+
+            //Dados do DEPENDENTE (1 para Muitos)
+            Console.Write("Número de Dependentes....: ");
+            var numDependentes = int.Parse(Console.ReadLine());
+
+            //Percorre o número de dependentes informados para cadastro da lista
+            for (int i = 0; i < numDependentes; i++)
+            {
+                var dependente = new Dependente();
+
+                dependente.IdDependente = Guid.NewGuid();
+
+                Console.Write($"\nENTRE com o {i + 1}º DEPENDENTE: \n");
+                
+                Console.Write("Nome do Dependente.......: ");
+                dependente.Nome = Console.ReadLine();
+
+                Console.Write("Data de Nascimento.......: ");
+                dependente.DataNascimento = DateTime.Parse(Console.ReadLine());
+
+                //Adicionar objeto dependente em Funcionario.Depentende
+                funcionario.Dependentes.Add(dependente);
+            }
+
+            //Grava os Dados do Funcionario em um arquivo json
+            var funcionarioRepository = new FuncionarioRepository();
+            funcionarioRepository.ExportarJSON(funcionario);
+
+            Console.Write("\nARQUIVO JSON GERADO COM SUCESSO.\n");
+        }
+    }
+}
---
+++ Projeto02/Repositories/FuncionarioRepository.cs
@@ -0,0 +1,33 @@
+﻿using Newtonsoft.Json;
+using Projeto02.Entities;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projeto02.Repositories
+{
+    public class FuncionarioRepository
+    {
+        public void ExportarJSON(Funcionario funcionario)
+        {
+            //Define Path onde será gravado o arquivo
+            var path = $"c:\\temp\\funcionario_{funcionario.IdFuncionario}.json";
+
+            //Abre o arquivo para gravação
+            var streamWriter = new StreamWriter(path);
+
+            //Serializa os dados de Funcionario em JSON
+            var json = JsonConvert.SerializeObject(funcionario,Formatting.Indented);
+
+            //Escreve os dados serializados no arquivo
+            streamWriter.WriteLine(json);
+
+            //Fecha o arquivo
+            streamWriter.Close();
+
+        }
+    }
+}
